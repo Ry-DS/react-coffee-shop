3d8b6ed3916dbf8a597b63b5bfa39c335bb2350e
Ryan.S <ryan2002.rs@gmail.com>
2019-02-11 23:17:11 +1100

    Added Milk and Carousel titles

--- src/App.js
+++ src/App.js
@@ -8,7 +8,15 @@ import './App.css';
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = {data: []};
+        this.state = {order: []};
+        this.state.order['type'] = 'Latte';
+        this.prices = {
+            type: {flat_white: 2.5, cappuccino: 3, short_black: 2, latte: 3},
+            milk: {full_cream_milk: 0, skim_milk: 0.5, soy_milk: 1},
+            size: {small: 0, regular: 0.5, large: 1},
+            sugar: {none: 0, one: 0.05, two: 0.1}
+
+        };
         setTimeout(() => {
             $('#chooserCarousel').carousel({interval: false, keyboard: false});
         }, 50);
@@ -18,13 +26,14 @@ class App extends Component {
     }
 
     handleButtonClick(type, value) {
-        const data = this.state.data.slice();
-        data[type] = value;
-        this.setState({...this.state, data: data})
+        const order = this.state.order.slice();
+        order[type] = value;
+        this.setState({...this.state, order: order})
 
     }
   render() {
-      const type = this.state.data['type'];
+      const type = this.state.order.type;
+      const milk = this.state.order.milk;
     return (
      <div>
          <div className="welcome">
@@ -34,7 +43,7 @@ class App extends Component {
              <div className="row">
                  <div className="col-sm-3 text-center overview">Selections
                      <div className="selections-text">
-                         <h4>{type !== undefined ? "Type: " + type : ""}</h4>
+                         <h4 className="welcome">{type !== undefined ? "Type: " + type : ""}</h4>
                      </div>
                      <div className="coffee-container text-center">
                      <div className="saucer">
@@ -42,7 +51,7 @@ class App extends Component {
                          <div className="cup">
                              <div className={type === undefined ? "empty" : "coffee " + type.toLowerCase()}>
                                  <div
-                                     className={type !== undefined && type.includes("Hot-Chocolate") ? "marshmallow" : ""}/>
+                                     className={type !== undefined && type.includes("Hot_Chocolate") ? "marshmallow" : ""}/>
                                  <div className="shine"></div>
                                  <div className="shine left"></div>
                                  <div className="foam"></div>
@@ -63,25 +72,26 @@ class App extends Component {
                          </ol>
                          <div className="carousel-inner">
                              <div className="carousel-item active">
+                                 <h1 className="welcome text-center" style={{color: 'white'}}>Choose a Type</h1>
                                  <div className="container">
                                      <div className="row row-padding">
-                                         <div className="col-sm-6 button-padding-left"><Button label="Americano"
+                                         <div className="col-sm-6 button-padding-left"><Button label="Flat White"
                                                                                                onClick={() => this.handleButtonClick('type', 'Americano')}
-                                                                                               className="p-button-secondary type-button"/>
+                                                                                               className={(type === 'Americano' ? "p-button-success" : "p-button-secondary") + " type-button"}/>
                                          </div>
                                          <div className="col-sm-6 button-padding-right"><Button label="Latte"
                                                                                                 onClick={() => this.handleButtonClick('type', 'Latte')}
-                                                                                                className="p-button-secondary type-button"/>
+                                                                                                className={(type === 'Latte' ? "p-button-success" : "p-button-secondary") + " type-button"}/>
                                          </div>
                                      </div>
                                      <div className="row row-padding">
-                                         <div className="col-sm-6 button-padding-left"><Button label="Hot Chocolate"
-                                                                                               onClick={() => this.handleButtonClick('type', 'Hot-Chocolate')}
-                                                                                               className="p-button-secondary type-button"/>
+                                         <div className="col-sm-6 button-padding-left"><Button label="Cappuccino"
+                                                                                               onClick={() => this.handleButtonClick('type', 'Hot_Chocolate')}
+                                                                                               className={(type === 'Hot_Chocolate' ? "p-button-success" : "p-button-secondary") + " type-button"}/>
                                          </div>
-                                         <div className="col-sm-6 button-padding-right"><Button label="Water"
+                                         <div className="col-sm-6 button-padding-right"><Button label="Short Black"
                                                                                                 onClick={() => this.handleButtonClick('type', 'Water')}
-                                                                                                className="p-button-secondary type-button"/>
+                                                                                                className={(type === 'Water' ? "p-button-success" : "p-button-secondary") + " type-button"}/>
                                          </div>
                                      </div>
 
@@ -89,7 +99,28 @@ class App extends Component {
 
                              </div>
                              <div className="carousel-item">
-                                 <Button label="This Milk" className="p-button-secondary"/>
+                                 <div className="container">
+                                     <h1 className="welcome text-center" style={{color: 'white'}}>Choose a Milk
+                                         Type</h1>
+                                     <div className="row row-padding">
+                                         <div className="col-sm-6 button-padding-left"><Button label="Full Cream Milk"
+                                                                                               onClick={() => this.handleButtonClick('milk', 'Full_Cream_Milk')}
+                                                                                               className={(milk === 'Full_Cream_Milk' ? "p-button-success" : "p-button-secondary") + " type-button"}/>
+                                         </div>
+                                         <div className="col-sm-6 button-padding-right"><Button label="Skim Milk"
+                                                                                                onClick={() => this.handleButtonClick('milk', 'Skim_Milk')}
+                                                                                                className={(milk === 'Skim_Milk' ? "p-button-success" : "p-button-secondary") + " type-button"}/>
+                                         </div>
+                                     </div>
+                                     <div className="row row-padding">
+                                         <div className="col-sm-6 button-padding-left"><Button label="Soy Milk"
+                                                                                               onClick={() => this.handleButtonClick('milk', 'Soy_Milk')}
+                                                                                               className={(milk === 'Soy_Milk' ? "p-button-success" : "p-button-secondary") + " type-button"}/>
+                                         </div>
+
+                                     </div>
+
+                                 </div>
                              </div>
                              <div className="carousel-item">
                                  <Button label="This Size" className="p-button-secondary type-button"/>
