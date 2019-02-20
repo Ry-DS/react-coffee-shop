05603c64a15b8f37fc8076605653e50dc472d693
Ryan.S <ryan2002.rs@gmail.com>
2019-02-20 12:43:50 +1100

    Fixed issue regarding element keys in an array

--- src/ButtonSection.js
+++ src/ButtonSection.js
@@ -22,7 +22,7 @@ class ButtonSection extends Component {
 
             } else entry = rows[rows.length - 1];
             let buttonData = this.data[keys[i]];
-            entry.push(<div className={"col-sm-6 button-padding-" + (!isOdd(i) ? "right" : "left")}><Button
+            entry.push(<div key={i} className={"col-sm-6 button-padding-" + (!isOdd(i) ? "right" : "left")}><Button
                     label={buttonData.name}
                     onClick={() => this.onClick(buttonData.code)}
                     className={(this.props.selected === buttonData ? "p-button-success" : "p-button-secondary") + " type-button"}/>
@@ -33,9 +33,9 @@ class ButtonSection extends Component {
 
         }
         let htmlRows = [];
-        rows.forEach(row => {
-            htmlRows.push(React.createElement('div', {className: 'row row-padding'}, row));
-        });
+        for (let i = 0; i < rows.length; i++) {
+            htmlRows.push(React.createElement('div', {className: 'row row-padding', key: i}, rows[i]));
+        }
 
 
         return (
